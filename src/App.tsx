bef62621917d780ef294a8c286dd5867529910c1
Lucas Boaratti <lucasboaratti15@gmail.com>
2025-08-12 16:52:23 -0300

    Adicionando a home para exibir junto com o cabeçalho.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import { Cabecalho } from "./Components/Cabeçalho/Cabecalho";
+import { Home } from "./Pages/Home/Home";
 
 function App() {
 	return (
-		<Cabecalho/>
+		<>
+			<Cabecalho/>
+			<Home/>
+		</>
 	);
 }
 
