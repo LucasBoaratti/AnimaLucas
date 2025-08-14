07a7a7d7c6ab9931ead2f67444b80c8b595c3a67
Lucas Boaratti <lucasboaratti15@gmail.com>
2025-08-14 16:43:11 -0300

    Rederizando o rodapé pela página.

--- src/Pages/Index.tsx
+++ src/Pages/Index.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import { Cabecalho } from "../Components/Cabeçalho/Cabecalho";
+import { Rodape } from "../Components/Rodapé/Rodape";
 
 export function Index() {
     return (
         <>
             <Cabecalho/>
             <Outlet/>
+            <Rodape/>
         </>
     );
 }
