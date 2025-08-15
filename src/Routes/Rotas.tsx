a00ee1c9fb0a47ff35553733173c280bcabe34e9
Lucas Boaratti <lucasboaratti15@gmail.com>
2025-08-15 00:04:54 -0300

    Rota da página de login adicionada.

--- src/Routes/Rotas.tsx
+++ src/Routes/Rotas.tsx
@@ -1,11 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import { Index } from "../Pages/Index";
 import { Home } from "../Pages/Home/Home";
+import { Login } from "../Pages/Login/Login";
 
 export function Rotas() {
     return (
         <Routes>
-            <Route path="/" element={<Index/>}>
+            {/* Rota para a página de login */}
+            <Route path="/">
+                <Route index element={<Login/>}/>
+            </Route>
+
+            {/* Rota para a página home */}
+            <Route path="/home" element={<Index/>}>
                 <Route index element={<Home/>}/>
             </Route>
         </Routes>
